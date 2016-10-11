93ca175a497453ae34c06c53d31fe3c57485aaab
Descent098 <kieranw098@gmail.com>
2016-10-10 23:54:50 -0600

    Changed Font Color

--- game_main.js
+++ game_main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'World_Hello', { create: create });
+var game = new Phaser.Game(1366, 768, Phaser.CANVAS, 'World_Hello', { create: create });
 
 function preload() {
     
@@ -6,8 +6,8 @@ function preload() {
 
 function create() {
 
-    var text = "- I am snek, Hear me Hiss";
-    var style = { font: "45px Arial", fill: "#008000", align: "center" };
+    var text = "I am snek, Hear me Hiss";
+    var style = { font: "40px Times New Roman", fill: "#800000", align: "center" };
 
     var t = game.add.text(game.world.centerX-300, 0, text, style);
     
